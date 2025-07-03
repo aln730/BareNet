6b4cd92572dfc9e91847da8d831defbd52b286c0
aln730 <arnsg730@proton.me>
2025-07-02 21:58:58 -0400

    GDT has been kind of implemented; I want a minimal setup. Let's see how it goes

--- src/arch/x86_64/gdt.c
+++ src/arch/x86_64/gdt.c
@@ -1,13 +1,20 @@
 #include "gdt.h"
 
-
+// GDT entry struct (packed)
 struct gdt_entry{
-    // GDT entry struct (packed)
-}
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_mid;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
+} __attribute__((packed));
 
+// GDTR struct
 struct gdt_ptr{
-    // GDTR struct
-}
+    uint16_t limit;
+    uint64_t base;
+} __attribute__((packed));
 
 // GDT table with 3 entries: null, code, data
 static struct gdt_entry gdt[3];
@@ -15,10 +22,22 @@ static struct gdt_ptr gdtr;
 
 extern void load_gdt(struct gdt_ptr* gdtr);
 
-void gdt_set_entry(){
-
+void gdt_set_entry(int i, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran) {
+    gdt[i].limit_low = limit & 0xFFFF;
+    gdt[i].base_low = base & 0xFFFF;
+    gdt[i].base_mid = (base >> 16) & 0xFF;
+    gdt[i].access = access;
+    gdt[i].granularity = ((limit >> 16) & 0x0F) | (gran & 0xF0);
+    gdt[i].base_high = (base >> 24) & 0xFF;
 }
 
 void gdt_init(void) {
-    // TODO: Define GDT entries and load GDT
+    gdtr.limit = sizeof(gdt) - 1;
+    gdtr.base = (uint64_t)&gdt;
+
+    gdt_set_entry(0, 0, 0, 0, 0);                // Null
+    gdt_set_entry(1, 0, 0xFFFFF, 0x9A, 0xA0);    // Code
+    gdt_set_entry(2, 0, 0xFFFFF, 0x92, 0xA0);    // Data
+
+    load_gdt(&gdtr);
 }
