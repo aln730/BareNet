b0612442ba37b0841bf241c2fa384189fef1e871
aln730 <arnsg730@proton.me>
2025-07-02 21:48:48 -0400

    committing stuff cus I dont wanna fuck up anyhting

--- src/arch/x86_64/gdt.c
+++ src/arch/x86_64/gdt.c
@@ -1,5 +1,24 @@
 #include "gdt.h"
 
+
+struct gdt_entry{
+    // GDT entry struct (packed)
+}
+
+struct gdt_ptr{
+    // GDTR struct
+}
+
+// GDT table with 3 entries: null, code, data
+static struct gdt_entry gdt[3];
+static struct gdt_ptr gdtr;
+
+extern void load_gdt(struct gdt_ptr* gdtr);
+
+void gdt_set_entry(){
+
+}
+
 void gdt_init(void) {
     // TODO: Define GDT entries and load GDT
 }
